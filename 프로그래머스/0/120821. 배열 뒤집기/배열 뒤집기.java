d9fb728621d6d9d2d8f6f615b81c95aff8c1167e
stduyhh <ghls1252@gmail.com>
2025-10-02 22:50:27 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] result = new int[num_list.length];
+
+        for (int i = num_list.length-1, j = 0; i >= 0; i--, j++) {
+            result[j] = num_list[i];
+        }
+
+        return result;
+    }
+}
