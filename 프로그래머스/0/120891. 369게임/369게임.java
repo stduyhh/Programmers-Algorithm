0f94483f3c68f04a48520c6285c8f00f19e6cb7b
stduyhh <ghls1252@gmail.com>
2026-01-08 19:39:39 +0900

    [level 0] Title: 369게임, Time: 0.16 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,16 @@
+import javax.swing.plaf.basic.BasicIconFactory;
+
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+
+        String[] arr = Integer.toString(order).split("");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("3") || arr[i].equals("6") || arr[i].equals("9")) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
